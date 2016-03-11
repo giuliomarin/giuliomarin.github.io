a6cd14dab117a33a4590d05fe546384aad42ea34
Giulio Marin <giulio.marin@me.com>
2016-03-11 16:00:55 +0100

    download publications from scholar

---
+++ scripts/downloadwebpages.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+# Download page from google scholar
+
+wget -O - "https://scholar.google.com/citations?user=t9UpeEAAAAAJ&hl=en" > /home/maringiu/Public-Htdocs/publications/googlescholar_giuliomarin.html
