5cb74b3c2a569b73b1b544e21b8d66deb21b337c
Giulio Marin <giulio.marin@me.com>
2016-03-22 17:43:27 +0100

    auto citations and runtastic

--- scripts/downloadwebpages.sh
+++ scripts/downloadwebpages.sh
@@ -2,3 +2,5 @@
 # Download page from google scholar
 
 wget -O - "https://scholar.google.com/citations?user=t9UpeEAAAAAJ&hl=en" > /home/maringiu/Public-Htdocs/publications/googlescholar_giuliomarin.html
+
+wget -O - "https://www.runtastic.com/en/users/Giulio-Marin/statistics" > /home/maringiu/Public-Htdocs/extra/runtastic_giuliomarin.html
