760ca5911e948e40323daff141892e6c9813b61d
Giulio Marin <giulio.marin@me.com>
2016-04-17 00:21:23 +0200

    updated copyright

--- autogeneration/updateCopyright.py
+++ autogeneration/updateCopyright.py
@@ -8,7 +8,7 @@ import csv
 import sys
 from datetime import date
 
-COPYRIGHT_STRING = r'<p>&#169;%s&nbsp;<a href="index.html">Giulio Marin</a></p>'
+COPYRIGHT_STRING = r'<p>&#169;%s&nbsp;<a href="%sindex.html">Giulio Marin</a></p>'
 CURRENT_YEAR = date.today().year
 
 if __name__ == '__main__':
@@ -19,7 +19,8 @@ if __name__ == '__main__':
                 continue
             currFile = os.path.join(dirname, filename)
             fileContent = open(currFile, 'r').read()
-            fileContent = fileContent.replace(COPYRIGHT_STRING % str(CURRENT_YEAR-1), COPYRIGHT_STRING % CURRENT_YEAR)
+            for prevFold in ['../', '../../']:
+                for prevYear in range(1,10):
+                    fileContent = fileContent.replace(COPYRIGHT_STRING % (str(CURRENT_YEAR - prevYear), prevFold), COPYRIGHT_STRING % (CURRENT_YEAR, prevFold))
             open(currFile, 'w').write(fileContent)
             print 'File updated: %s' % filename
-        
