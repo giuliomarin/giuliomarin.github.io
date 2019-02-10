739671d4530366237893a113aa2e12a9a4259c96
Giulio Marin <giulio.marin@me.com>
2019-02-10 00:18:58 -0800

    Add get OS function

--- wedding/script.js
+++ wedding/script.js
@@ -127,3 +127,26 @@ var layOutDay = (events, day, hours) => {
     myNode.appendChild(createEvent(event, height, top, left, units));
   });
 }
+
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+      platform = window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+      os = "null";
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'MacOS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
